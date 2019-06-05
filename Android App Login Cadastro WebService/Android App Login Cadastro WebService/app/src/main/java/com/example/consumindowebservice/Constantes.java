aaa85ce51c7294ef463170bb8bd78022b7c2ca89
Talles929 <prlptss@gmail.com>
2019-06-05 18:28:21 -0300

    Add files via upload

---
+++ Android App Login Cadastro WebService/Android App Login Cadastro WebService/app/src/main/java/com/example/consumindowebservice/Constantes.java
@@ -0,0 +1,10 @@
+package com.example.consumindowebservice;
+
+
+
+public class Constantes {
+
+    
+    public final static String urlLogin = "http://192.168.1.101:8090/WebServiceAndroid/login.php";
+    public final static String urlRegistrar = "http://192.168.1.101:8090/WebServiceAndroid/registrar.php";
+}
