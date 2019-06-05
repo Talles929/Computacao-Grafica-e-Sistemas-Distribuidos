cf5c4c0de4bea681726468e589f26a83549cd631
Talles929 <prlptss@gmail.com>
2019-06-05 18:53:26 -0300

    Delete gerarJSON.php

--- Android App Login Cadastro WebService/Android App Login Cadastro WebService/WebServiceAndroid PHP/Funcoes/gerarJSON.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-function gerarArrayJson($status, $msg, $dados){
-    $array = array("status_requisicao"=> $status, "msg" => $msg, "retorno" => $dados);
-    return json_encode($array);
-}
